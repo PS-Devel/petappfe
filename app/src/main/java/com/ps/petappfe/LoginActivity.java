ad696fb47e02a6a3ec76bfc194620485b2788951
Luigi Staglianò <l.stagliano@gmail.com>
2022-02-21 12:15:54 +0100

    Creazione app android; aggiunta sezione autenticazione;

---
+++ app/src/main/java/com/ps/petappfe/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.ps.petappfe;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+public class LoginActivity extends Fragment {
+
+
+    public LoginActivity() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_login, container, false);
+    }
+
+}
